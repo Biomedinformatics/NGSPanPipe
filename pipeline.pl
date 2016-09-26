c96850883c1300511fa1d41332acf22a6276684d
Biomedinformatics <kulsum.umay@gmail.com>
2016-09-26 16:08:42 +0530

    Update pipeline.pl

--- pipeline.pl
+++ pipeline.pl
@@ -6,4 +6,4 @@ system "perl comp.pl *.fq.out.out1 >compare.txt";
 system "perl collapse1.pl 2"; # 2 can be replaced by the filter cutoff user wants to specify i.e.total number of occurrence of reads in all strains....we put 2 in case of simulated reads.
 system "perl align2.pl"; # open align.pl and change the reference file name according to the genome of interest.
 system "perl ptt_try.pl NC_000962.fna >ptt.txt"; # the reference ptt file name has to be changed as user's requirement.
-system "perl ptt_count.pl >count.txt";
+system "perl ptt_count.pl >count.txt"; #output file giving the proteins covered by sequenced reads with their location in the genome.
