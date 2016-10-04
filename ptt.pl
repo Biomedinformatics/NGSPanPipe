a20624e343b553b1e98358c465554c6556e47b1a
Biomedinformatics <kulsum.umay@gmail.com>
2016-10-04 14:35:25 +0530

    Update and rename ptt_try.pl to ptt.pl

--- ptt.pl
+++ ptt.pl
@@ -2,28 +2,20 @@ use strict;
 use warnings;
 
 open my $m, '<', 'mapped.txt' or die 'Cannot open mapped.txt';
-
 while (<$m>) {
-chomp;
-   
-    my @l = split /\t/;
- 
-
-    open my $p, '<', $ARGV[0] or die 'Cannot open $file2';
-
-    while (<$p>) {
-    next if $. == 1;
-    next if $. == 2;
-    next if $. == 3;
-
-       $_=~ s/\r//;
     chomp;
-    
-        
+    my @l = split /\t/;
+my @seq = split(/\|/, $l[0]);
+my $name = $seq[3];
+    open my $p, '<', 'NC_000962.ptt' or die 'Cannot open NC_000962.ptt';
+    while (<$p>) {
+        next if /^Location/;
+
+        chomp;
         my (@range) = split(/\t/, $_);
         my ($fm, $to) = split(/\.\./, $range[0]);
         if ($l[3] >= $fm and $l[3] <= $to)
-           { print $l[3]."\t".$range[0]."\t".$range[8]."\t".$l[9]."\n";
+           { print $l[3]."\t".$range[0]."\t".$range[8]."\t".$l[9]."\t".$name."\n";
         }
     }
     close $p;
