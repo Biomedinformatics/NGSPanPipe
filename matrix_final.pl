4e7acdf94e1c234cf3470607ca28082f51c1cc38
Biomedinformatics <kulsum.umay@gmail.com>
2016-10-05 13:21:35 +0530

    Update matrix_final.pl

--- matrix_final.pl
+++ matrix_final.pl
@@ -1,3 +1,14 @@
+opendir(DIR, ".");
+@files = grep(/\.fq$/,readdir(DIR));
+closedir(DIR);
+
+foreach $file (sort @files) {
+@file1 = split /\./, $file;
+$file2 = $file1[0];
+$file3 = $file3."\t".$file2;
+}
+print "Position"."\t"."Protein name".$file3."\n";
+
 open (FH, "matrix1.txt");
 while ($seq = <FH>)
 { 
