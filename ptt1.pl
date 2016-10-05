5f6cec5f86ced04c2d483f0948321ab2b7efce03
Biomedinformatics <kulsum.umay@gmail.com>
2016-10-05 13:07:37 +0530

    Update ptt1.pl

--- ptt1.pl
+++ ptt1.pl
@@ -9,9 +9,10 @@ while (my $line=<>) {
     my @F=split(/\t/,$line);
 
 
-   
+    $k{$F[1]}{$F[2]}=1;
+    $k{$F[1]}{$F[4]}=1;
     
-    $k{$F[2]}{$F[4]}=1;
+    
     
 
 }
@@ -24,7 +25,7 @@ my @names=(sort keys(%k));
 foreach my $name (@names) {
 chomp $name;
     print "$name\t";
-    foreach my $value ( (keys(%{$k{$name}})) ){
+    foreach my $value ( (sort keys(%{$k{$name}})) ){
 chomp $value;
       print "\t$value";
 
