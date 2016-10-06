351fc1c3fa74a01a72fcc5829d872b2fa4dc5a4e
Biomedinformatics <kulsum.umay@gmail.com>
2016-10-06 10:52:50 +0530

    Update pipeline.pl

--- pipeline.pl
+++ pipeline.pl
@@ -7,7 +7,8 @@ system "perl collapse1.pl 2"; # 2 can be replaced by the filter cutoff user want
 system "perl align2.pl"; # open align.pl and change the reference file name according to the genome of interest.
 system "perl ptt.pl NC_000962.fna >ptt.txt"; # the reference ptt file name has to be changed as user's requirement.
 system "perl ptt_count.pl >count.txt"; #output file giving the proteins covered by sequenced reads with their location in the genome.
-system "perl ptt1.pl ptt.txt >p.txt";
+system "perl ptt1.pl ptt.txt >p1.txt";
+system "perl ptt2.pl >p.txt";
 system "perl matrix.pl >matrix.txt";
 system "perl matrix1.pl matrix.txt >matrix1.txt";
 system "perl matrix_final.pl >panmatrix.txt"; #presence/absence of genes in specific strains in binary format.
