75dde3aa4d990b2e4065ab906536637fd239082c
Biomedinformatics <kulsum.umay@gmail.com>
2016-10-06 14:31:02 +0530

    Update matrix.pl

--- matrix.pl
+++ matrix.pl
@@ -1,38 +1,39 @@
-opendir(DIR, ".");
-@files = grep(/\.fq$/,readdir(DIR));
-closedir(DIR);
-foreach $file (@files) {
-@file1 = split /\./, $file;
-$file2 = $file1[0];
-$file3 = $file3."\t".$file2;
-}
-@file4 = split /\t/, $file3;
-$size = @file4;
+#!/usr/bin/perl 
 
-open (FH, "p.txt");
-while ($seq = <FH>)
+ 
+my %k;
+
+
+while (my $line=<>) {
+    
+    my @F=split(/\t/,$line);
+$size= @F;
+for ($i=1;$i<=$size;$i++)
 {
-@seq = split /\t/, $seq;
-$size1 = @seq;
-for ($i=1; $i<=$size+1; $i++){
-chomp @seq[$size1-1];
-print @seq[0]."\t".@seq[$size1-1]."\t";
- @seq1 = split /\./, @seq[$i];
-$seq2= @seq1[0];
-chomp $seq2;
-for ($j=1; $j<=$size-1; $j++){
-if (@file4[$j] eq "$seq2"){
-print @file4[$j].":1"."\t";}
-else{
-print @file4[$j].":0"."\t";}
-}
 
-print "\n";
+   
+    $k{$F[0]}{$F[$i]}=1;
+    $k{$F[0]}{$F[1]}=1;
 }
-
 }
 
 
+my @names=(sort keys(%k));
+
+
+
+foreach my $name (@names) {
+chomp $name;
+    print "$name";
+    foreach my $value ( (sort keys(%{$k{$name}})) ){
+chomp $value;
+      print $value."\t";
+
+    } 
+  print "\n"; 
+    
+
+}
 
 
 
