9d7ff7906acbdf81eba20c09ab17fda37434b4cd
Biomedinformatics <kulsum.umay@gmail.com>
2016-10-19 15:36:31 +0530

    Update pipeline.pl

--- pipeline.pl
+++ pipeline.pl
@@ -4,11 +4,11 @@ system "perl prepare.pl *.fq";   # input file should be reads in fastq format.
 system "perl count1.pl *.fq.out";
 system "perl comp.pl *.fq.out.out1 >compare.txt";
 system "perl collapse1.pl 2"; # 2 can be replaced by the filter cutoff user wants to specify i.e.total number of occurrence of reads in all strains....we put 2 in case of simulated reads.
-system "perl align2.pl"; # open align.pl and change the reference file name according to the genome of interest.
-system "perl ptt.pl NC_000962.fna >ptt.txt"; # the reference ptt file name has to be changed as user's requirement.
+system "perl align2.pl"; # open align.pl and change the reference file name according to the genome of interest and the reference file should be present in the folder.
+system "perl ptt.pl >ptt.txt"; # the reference ptt file name has to be changed as user's requirement and the ptt file must be present in the folder.
 system "perl ptt_count.pl >count.txt"; #output file giving the proteins covered by sequenced reads with their location in the genome.
 system "perl ptt1.pl ptt.txt >p.txt";
 system "perl matrix.pl >matrix.txt";
 system "perl matrix1.pl matrix.txt >matrix1.txt";
-system "perl matrix_final.pl >final.txt"; #presence/absence of genes in specific strains in binary format.
-system "perl ptt2.pl >panmatrix.txt";
+system "perl matrix_final.pl >final.txt"; 
+system "perl ptt2.pl >panmatrix.txt"; #presence/absence of genes in specific strains in binary format.
