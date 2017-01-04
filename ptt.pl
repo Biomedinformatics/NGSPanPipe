97194b4ce51d849fb7c4e2339936d5ca8097192c
Biomedinformatics <kulsum.umay@gmail.com>
2017-01-04 11:26:29 +0530

    Update ptt.pl

--- ptt.pl
+++ ptt.pl
@@ -1,13 +1,15 @@
 use strict;
 use warnings;
-
+my @files = @ARGV;
+my $files = shift;
+open (FA, ">ptt.txt");
 open my $m, '<', 'mapped.txt' or die 'Cannot open mapped.txt';
 while (<$m>) {
     chomp;
     my @l = split /\t/;
 my @seq = split(/\|/, $l[0]);
 my $name = $seq[3];
-    open my $p, '<', 'NC_000962.ptt' or die 'Cannot open NC_000962.ptt';
+    open my $p, '<', $files or die 'Cannot open .ptt';
     while (<$p>) {
         next unless $. > 3;
 
@@ -15,7 +17,7 @@ my $name = $seq[3];
         my (@range) = split(/\t/, $_);
         my ($fm, $to) = split(/\.\./, $range[0]);
         if ($l[3] >= $fm and $l[3] <= $to)
-           { print $l[3]."\t".$range[0]."\t".$range[8]."\t".$l[9]."\t".$name."\n";
+           { print FA $l[3]."\t".$range[0]."\t".$range[8]."\t".$l[9]."\t".$name."\n";
         }
     }
     close $p;
