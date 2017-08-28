a85ab38a093e05ba4e8d7c676666dc3e6db6c2a3
Biomedinformatics <kulsum.umay@gmail.com>
2017-08-28 10:19:47 +0530

    Update prepare.pl

--- prepare.pl
+++ prepare.pl
@@ -2,23 +2,8 @@
     use warnings;
 my @files= @ARGV;
    for my $input_file (@files) {
-   my $output_file = $input_file.".out";
-    process_file($input_file, $output_file);
-}
-sub process_file {
-    my($input_file, $output_file) = @_;  
+system "sed -i '1s/^/string/' $input_file";
+  system "sed -i '0~4 s/\$/string/g' $input_file";
     
-
-    my $file = shift or die "Usage: $0 FILE\n";
-    open my $fh, '<', $file or die "Could not open '$file' $!";
-    open my $fa, '>', $output_file;
-
-    while (my $line = <$fh>) {
-$line =~ s/\@/\@\|\|\|$file\|/g;
-$line =~ s/\@/\n\@/g;
-
-   print $fa $line;
 }
-
-}
-
+ 
