5ba3f80f11e065a1586d96f397d51b0831234728
Biomedinformatics <kulsum.umay@gmail.com>
2017-08-28 10:18:15 +0530

    Update matrix_final-nt.pl

--- matrix_final-nt.pl
+++ matrix_final-nt.pl
@@ -1,6 +1,6 @@
-
-@files = @ARGV;
-
+opendir(DIR, ".");
+@files = grep(/\.fq$/,readdir(DIR));
+closedir(DIR);
 
 foreach $file (sort @files) {
 @file1 = split /\./, $file;
@@ -8,9 +8,9 @@ $file2 = $file1[0];
 $file3 = $file3."\t".$file2;
 }
 
+print "gene".$file3."\n";
 
-
-open (FH, "matrix1-nt.txt");
+open (FH, "matrix1.txt");
 while ($seq = <FH>)
 { 
 @seq = split (/\t/, $seq);
