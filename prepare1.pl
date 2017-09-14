98a4ec7bb586516f75994e55d9c62b57cfa33f9e
Biomedinformatics <kulsum.umay@gmail.com>
2017-09-14 14:19:43 +0530

    Update prepare1.pl

--- prepare1.pl
+++ prepare1.pl
@@ -1,9 +1,18 @@
 use strict;
-    use warnings;
+    no warnings;
 my @files= @ARGV;
    for my $input_file (@files) {
+if ($input_file == "*.gz")
+{
+my @outfile = split (/.gz/, $input_file);
+my $out = $outfile[0];
 
-   my $output_file = $input_file.".out";
+   my $output_file = $out.".out";
+    process_file($out, $output_file);
+}
+else
+{
+my $output_file = $input_file.".out";
     process_file($input_file, $output_file);
 }
 sub process_file {
@@ -21,6 +30,6 @@ $line =~ s/$file\|\n/$file\|/g;
 
    print $fa $line;
 }
-
 }
 
+}
