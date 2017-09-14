d343becd6f6f7f920df4ee76354b5ca063c46c26
Biomedinformatics <kulsum.umay@gmail.com>
2017-09-14 14:18:57 +0530

    Update prepare.pl

--- prepare.pl
+++ prepare.pl
@@ -1,9 +1,18 @@
  use strict;
-    use warnings;
+    no warnings;
 my @files= @ARGV;
+
    for my $input_file (@files) {
+if ($input_file == "*.gz")
+{system "gunzip $input_file";
+my @outfile = split (/.gz/, $input_file);
+my $out = $outfile[0];
+
+system "sed -i '1s/^/string/' $out";
+  system "sed -i '0~4 s/\$/string/g' $out";}
+else {
 system "sed -i '1s/^/string/' $input_file";
-  system "sed -i '0~4 s/\$/string/g' $input_file";
-    
+system "sed -i '0~4 s/\$/string/g' $input_file";
 }
- 
+
+  }  
